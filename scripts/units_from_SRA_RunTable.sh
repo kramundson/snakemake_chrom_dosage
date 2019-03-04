337a64b98d750d036aaad218c9974ec40c364ceb
Kirk Amundson <kramundson@buffy.hpc.genomecenter.ucdavis.edu>
2019-03-04 12:01:15 -0800

    adapting for rice, init genome runs but SRA download slow without prefetch

---
+++ scripts/units_from_SRA_RunTable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# table for paired end read data, not used for karyotype analysis but nice to have on hand
+tail -n +2 Tang_etal_2018_GenomBiol_SRA.txt | \
+	awk -v OFS="\t" 'BEGIN {print "sample","unit","fq1","fq2","parhap"} {print $2,$6,$6"_1.fastq.gz",$6"_2.fastq.gz","NaN","haploid"}' > units_PE.tsv
+
+# table for forward mates only, used for karyotype analysis
+tail -n +2 Tang_etal_2018_GenomBiol_SRA.txt | \
+	awk -v OFS="\t" 'BEGIN {print "sample","unit","fq1","fq2","parhap"} {print $2,$6,$6"_1.fastq.gz","NaN","haploid"}' > units.tsv
