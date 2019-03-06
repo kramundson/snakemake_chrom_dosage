10d030197a4fa7196d8d3c7b152156a824cbffa4
Kirk Amundson <kramundson@buffy.hpc.genomecenter.ucdavis.edu>
2019-03-06 07:36:13 -0800

    rice pipeline running, todo update README

--- scripts/units_from_SRA_RunTable.sh
+++ scripts/units_from_SRA_RunTable.sh
@@ -2,7 +2,7 @@
 
 # table for paired end read data, not used for karyotype analysis but nice to have on hand
 tail -n +2 Tang_etal_2018_GenomBiol_SRA.txt | \
-	awk -v OFS="\t" 'BEGIN {print "sample","unit","fq1","fq2","parhap"} {print $2,$6,$6"_1.fastq.gz",$6"_2.fastq.gz","NaN","haploid"}' > units_PE.tsv
+	awk -v OFS="\t" 'BEGIN {print "sample","unit","fq1","fq2","parhap"} {print $2,$6,$6"_1.fastq.gz",$6"_2.fastq.gz","haploid"}' > units_PE.tsv
 
 # table for forward mates only, used for karyotype analysis
 tail -n +2 Tang_etal_2018_GenomBiol_SRA.txt | \
